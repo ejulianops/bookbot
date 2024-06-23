873c313ad57e7f179f49230768564dee6d6413a7
ejulianops <135237370+ejulianops@users.noreply.github.com>
2024-06-23 15:47:20 +0000

    complete bookbot

---
+++ main.py
@@ -0,0 +1,77 @@
+def main():
+    book_path = './books/frankenstein.txt'
+
+    # 8: Bookbot - Read file
+    text = read_book(book_path)
+    #print(text)
+    
+    # 9: Count words
+    words_count = count_words(text)
+    #print(words_count)
+
+    # 10: Count characters
+    characters_count = count_characters(text)
+    #print(characters_count)
+
+    # 11: Print a report
+    sorted_characters_count = get_sorted_list(characters_count)
+    report = print_report(book_path, words_count, sorted_characters_count)
+    print(report)
+
+
+
+# 8: Bookbot - Read file
+def read_book(text_file):
+    with open(text_file) as file:
+        return file.read()
+
+# 9: Count words
+def count_words(book):
+    split_text = book.split()
+    return len(split_text)
+
+# 10: Count characters
+def count_characters(book):
+    count = {}
+    for character in book:
+        if character.isalpha():
+            lower_alpha_character = character.lower()
+            if lower_alpha_character in count:
+                count[lower_alpha_character] += 1
+            else: 
+                count[lower_alpha_character] = 1
+    return count
+
+# 11: sort
+def include_keys(characters_dict):
+    key_value_characters_count = []
+    for char in characters_dict:
+        pair = {
+            'char': char,
+            'count': characters_dict[char],
+        }
+        key_value_characters_count.append(pair)
+    return key_value_characters_count
+
+def sort_characters(characters_dict):
+    return characters_dict['count']
+
+def get_sorted_list(characters_dict):
+    characters = include_keys(characters_dict)
+    characters.sort(reverse=True, key=sort_characters)
+    return characters
+
+def print_report(book_path, words_count, sorted_characters_list):
+    report = f"""
+    \n--- Begin report of {book_path[2:]} ---
+    \n{words_count} words found in the document
+    """
+
+    for character in sorted_characters_list:
+        report += f"\nThe '{character['char']}' character was found {character['count']} times"
+
+    report += "\n\n--- End report ---"
+    return report
+
+
+main()
